3607d01f2497e09c82b56f7ac1e98eef0d463050
Ricardo Pascua Jr <rdpascua@outlook.com>
2025-04-08 00:14:59 +0800

    fix invalid loadConfig, remove loader

--- js/index.js
+++ js/index.js
@@ -10,97 +10,93 @@ const IndexObj = {
     VOUCHER: "voucher",
     FIXACCOUNT: "fixaccount",
     PASS: "pass",
-  },
+},
 
-  init: function (data) {
+init: function (data) {
     this.loadJson(data);
-    setTimeout(function () {
-      $("#body_loading").addClass("hide");
-    }, 1000);
-
     this.initEvent();
-  },
+},
 
-  loadJson: function (data) {
+loadJson: function (data) {
     I18nObj.init(data);
     this.renderHtml(data);
-  },
+},
 
-  initEvent: function () {
+initEvent: function () {
     const self = this;
     $("#login_btn").on("click", function () {
       self.onLogin();
-    });
-  },
+  });
+},
 
-  renderHtml: function (data) {
+renderHtml: function (data) {
     const loginOptions = data?.custom_html?.login_options;
     this.loginOptions = loginOptions;
 
     if (!loginOptions) {
       return false;
-    }
+  }
 
     // Define priority order
-    const priorityOrder = [
+  const priorityOrder = [
       this.LOGIN_OPTION.VOUCHER,
       this.LOGIN_OPTION.FIXACCOUNT,
       this.LOGIN_OPTION.PASS,
-    ];
-    let currentOption = null;
+      ];
+  let currentOption = null;
 
     // Search for the default option according to the priority order
-    for (const option of priorityOrder) {
+  for (const option of priorityOrder) {
       if (loginOptions.includes(option)) {
         currentOption = option;
         this.currentOption = currentOption;
         break;
-      }
     }
+}
 
-    if (!currentOption) {
-      return false;
-    }
+if (!currentOption) {
+  return false;
+}
 
-    I18nObj.renderHtmlLang();
-    this.renderLoginHtml(loginOptions);
-    this.renderCurrentLogin(currentOption);
-  },
+I18nObj.renderHtmlLang();
+this.renderLoginHtml(loginOptions);
+this.renderCurrentLogin(currentOption);
+},
 
-  renderHtmlLang: function () {},
+renderHtmlLang: function () {},
 
-  renderLoginHtml: function (loginOptions) {
+renderLoginHtml: function (loginOptions) {
     // Control priority through styles: Voucher > Account > Oneclick
     if (loginOptions.length > 1) {
       $("#login_split_line").removeClass("hide");
-    }
+  }
 
-    const allOptions = [
+  const allOptions = [
       this.LOGIN_OPTION.VOUCHER,
       this.LOGIN_OPTION.FIXACCOUNT,
       this.LOGIN_OPTION.PASS,
-    ];
-    const missOptions = allOptions.filter(
+      ];
+  const missOptions = allOptions.filter(
       (option) => !loginOptions.includes(option)
-    );
+      );
 
-    missOptions.forEach((item) => {
+  missOptions.forEach((item) => {
       $(`.login-item-${item}`).remove();
-    });
-  },
+  });
+},
 
-  renderCurrentLogin(currentOption) {
+renderCurrentLogin(currentOption) {
     switch (currentOption) {
-      case this.LOGIN_OPTION.PASS:
+    case this.LOGIN_OPTION.PASS:
         $(".login-form-title").text(I18nObj.$t("one_click_login"));
         $('.login-form-title').attr('data-i18n', 'one_click_login')
         break;
-      case this.LOGIN_OPTION.FIXACCOUNT:
+    case this.LOGIN_OPTION.FIXACCOUNT:
         $(".login-form-title").text(I18nObj.$t("account_login"));
         $('.login-form-title').attr('data-i18n', 'account_login')
         break;
-      case this.LOGIN_OPTION.VOUCHER:
-      default:
+    case this.LOGIN_OPTION.VOUCHER:
+    default:
         $(".login-form-title").text(I18nObj.$t("voucher_login"));
         $('.login-form-title').attr('data-i18n', 'voucher_login')
     }
@@ -113,42 +109,42 @@ const IndexObj = {
     loginOptions.forEach((loginOpt) => {
       if (loginOpt !== currentOption) {
         $(".other-btn-wrapper .login-item-" + loginOpt).removeClass("hide");
-      }
-    });
-  },
+    }
+});
+},
 
-  changeLoginOption(currentOption) {
+changeLoginOption(currentOption) {
     this.currentOption = currentOption;
     this.renderCurrentLogin(currentOption);
-  },
+},
 
-  onLogin() {
+onLogin() {
     let paramObj = {};
 
     switch (this.currentOption) {
-      case this.LOGIN_OPTION.FIXACCOUNT:
+    case this.LOGIN_OPTION.FIXACCOUNT:
         paramObj.account = $("#account_input").val();
         paramObj.password = $("#account_password").val();
         break;
-      case this.LOGIN_OPTION.VOUCHER:
+    case this.LOGIN_OPTION.VOUCHER:
         paramObj.account = $("#voucher_code").val();
-      case this.LOGIN_OPTION.PASS:
+    case this.LOGIN_OPTION.PASS:
         break;
     }
 
     const validRes = this.validateLoginForm();
     if (!validRes) {
       return false;
-    }
+  }
 
-    paramObj = {
+  paramObj = {
       lang: I18nObj.currentLang,
       authType: this.currentOption,
       sessionId: this._getParamVal("sessionId"),
       ...paramObj,
-    };
+  };
 
-    $.post({
+  $.post({
       url: "/api/auth/general",
       data: JSON.stringify(paramObj),
       contentType: "application/json",
@@ -156,80 +152,80 @@ const IndexObj = {
         console.log("Server Response:", response);
         if (response.success) {
           location.href = response.result.logonUrl;
-        } else {
+      } else {
           $("#login_msg").text(response.message);
-        }
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.error("Error:", textStatus, errorThrown);
-      },
-    });
+      }
   },
+  error: function (jqXHR, textStatus, errorThrown) {
+    console.error("Error:", textStatus, errorThrown);
+},
+});
+},
 
-  validateLoginForm() {
+validateLoginForm() {
     $("#login_msg").text("");
     if (!this.currentOption) {
       return true;
-    }
+  }
 
-    switch (this.currentOption) {
-      case this.LOGIN_OPTION.PASS:
-        break;
-      case this.LOGIN_OPTION.FIXACCOUNT:
-        return this.validateAccountForm();
-      case this.LOGIN_OPTION.VOUCHER:
-      default:
-        return this.validateVoucherForm();
-    }
+  switch (this.currentOption) {
+  case this.LOGIN_OPTION.PASS:
+    break;
+case this.LOGIN_OPTION.FIXACCOUNT:
+    return this.validateAccountForm();
+case this.LOGIN_OPTION.VOUCHER:
+default:
+    return this.validateVoucherForm();
+}
 
-    return true;
-  },
+return true;
+},
 
-  validateVoucherForm() {
+validateVoucherForm() {
     const voucherCode = $("#voucher_code").val().trim();
     if (!voucherCode) {
       $("#login_msg").text(I18nObj.$t("please_enter_access_code"));
       return false;
-    }
-    return true;
-  },
+  }
+  return true;
+},
 
-  validateAccountForm() {
+validateAccountForm() {
     const accountVal = $("#account_input").val().trim();
     const accountPwd = $("#account_password").val().trim();
     if (!accountVal) {
       $("#login_msg").text(I18nObj.$t("please_enter_account"));
       return false;
-    }
-    if (!accountPwd) {
+  }
+  if (!accountPwd) {
       $("#login_msg").text(I18nObj.$t("please_enter_pwd"));
       return false;
-    }
-    return true;
-  },
+  }
+  return true;
+},
 
-  _getParamVal(paras) {
+_getParamVal(paras) {
     try {
       const topUrl = decodeURI(window.top.location.href);
       const queryString = topUrl.split('?')[1];
       if (!queryString) {
         return null;
-      }
-  
-      const paraString = queryString.split('&');
-      const paraObj = {};
-      for (var i = 0; i < paraString.length; i++) {
+    }
+
+    const paraString = queryString.split('&');
+    const paraObj = {};
+    for (var i = 0; i < paraString.length; i++) {
         const pair = paraString[i].split('=');
         if (pair.length === 2) {
           paraObj[pair[0].toLowerCase()] = pair[1];
-        }
       }
+  }
   
-      const returnValue = paraObj[paras.toLowerCase()];
-      return returnValue !== undefined ? returnValue : null;
-    } catch (e) {
-      console.error("Error accessing top window URL:", e);
-      return null;
-    }
-  },
+  const returnValue = paraObj[paras.toLowerCase()];
+  return returnValue !== undefined ? returnValue : null;
+} catch (e) {
+  console.error("Error accessing top window URL:", e);
+  return null;
+}
+},
 };
